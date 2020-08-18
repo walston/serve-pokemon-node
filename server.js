c8b5c3af1cbc228b74bd1006021056b160096788
Nathan Walston <nathanielwalston@gmail.com>
2020-08-17 19:42:34 -0700

    Just move data access into sql.js as if it were a repository

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const Koa = require("koa");
 const Router = require("@koa/router");
-// const Sqlite = require("sqlite3").verbose();
-const QUERIES = require("./sql");
+const database = require("./sql");
 
 const app = new Koa();
 const router = new Router();
 
 router.get("/type-chart", async (ctx, next) => {
-  return new Promise((resolve) => {
-    return resolve(QUERIES.GET_EFFECTIVENESS_CHART);
-  }).then((rows) => {
+  return database.getTypechart().then((rows) => {
     ctx.response.statusCode = 200;
     ctx.response.body = rows;
     next();
--- sql.js
+++ sql.js
@@ -1,10 +1,23 @@
-const GET_EFFECTIVENESS_CHART = `SELECT att.name, def.name, damage_factor FROM type_efficacy
+const sqlite3 = require("sqlite3");
+const database = new sqlite3.Database("./data/pokedex.sqlite");
+
+function getTypechart() {
+  const QUERY = `SELECT att.name as attacking, def.name as target, damage_factor as damagePercentage
+    FROM type_efficacy
     JOIN (SELECT * FROM type_names WHERE local_language_id = 9)
-        AS att ON type_efficacy.damage_type_id = att.type_id
+      AS att ON type_efficacy.damage_type_id = att.type_id
     JOIN (SELECT * FROM type_names WHERE local_language_id = 9)
-        AS def ON type_efficacy.target_type_id = def.type_id
+      AS def ON type_efficacy.target_type_id = def.type_id
     ;`;
 
+  return new Promise((resolve, reject) => {
+    database.all(QUERY, (err, rows) => {
+      if (err) return reject(err);
+      resolve(rows);
+    });
+  });
+}
+
 module.exports = {
-  GET_EFFECTIVENESS_CHART,
+  getTypechart,
 };
